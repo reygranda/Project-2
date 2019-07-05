fc02e3a3332bdbbc370b8468ef149c0bb42c8884
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-05 14:21:41 -0400

    initialized firebase database and added a submit function

--- public/js/index.js
+++ public/js/index.js
@@ -13,109 +13,45 @@ firebase.initializeApp(config);
 // Create a variable to reference the database
 var database = firebase.database();
 
-database.ref().on("value", function(snapshot) {
-  console.log(snapshot);
+// Button for user login
+$("#submit").on("click", function(event) {
+  event.preventDefault();
 
+  // Grabs user input
+  var userName = $("#username").val().trim();
+  var age = $("#age").val().trim();
+
+  // Creates local "temporary" object for holding user data
+  var newUser = {
+    username: userName,
+    age: age
+  };
+
+  // Uploads user data to the firebase database
+  database.ref().push(newUser);
+
+  // Logs everything to console
+  console.log(newUser.username);
+  console.log(newUser.age);
+
+  alert("User successfully added");
+
+  // Clears all of the text-boxes
+  $("#username").val("");
+  $("#age").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var newUser = childSnapshot.val().username;
+  var newAge = childSnapshot.val().age;
+
+  // User Info
+  console.log(newUser);
+  console.log(newAge);
   
 });
 
 
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
-
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
-
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
-  }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
