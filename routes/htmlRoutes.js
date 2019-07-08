28b304c6ef8af7b2046c861783f6a02243d83966
Kung Fu Kenny <brianferreira74@yahoo.com>
2019-07-08 13:15:34 -0400

    test2

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,33 +1,24 @@
 var db = require("../models");
-
+console.log(db);
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-<<<<<<< HEAD
-    res.sendFile(path.join(__dirname + ""));
-=======
-    res.render('index', {
-      style: 'index.css',
-      title: 'Home'
+    res.render("index", {
+      style: "index.css",
+      title: "Home"
     });
->>>>>>> 5f7193e3530fecd43df2b4967516969507c06153
   });
 
   // Load example page and pass in an example by id
   app.get("/draft", function(req, res) {
-      res.render('draft', {
-        style: 'draft.css'
-      });
+    res.render("draft", {
+      style: "draft.css"
     });
+  });
   
   
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
   });
-<<<<<<< HEAD
 };
-
-=======
-}
->>>>>>> 5f7193e3530fecd43df2b4967516969507c06153
